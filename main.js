87684f0a3908ca2b8f274dffe1fb5c1b4b5b60bc
wictoriadrefelt <wictoria.soderlunddrefeldt@gmail.com>
2021-10-18 19:32:20 +0200

    added some js functions

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-var listOfProducts;
+let listOfProducts;
 
-/** Get products from the json file and store it in a gobal variable */
 function loadProducts() {
     fetch("./products.json")
     .then(function(response) {
@@ -9,23 +8,85 @@ function loadProducts() {
     .then(function(products) {
         listOfProducts = products;
         addProductsToWebpage();
+        console.log(addProductsToWebpage());
     });
 }
 
 
+
+
+
+
 function initSite() {
     loadProducts();
+    
     // This would also be a good place to initialize other parts of the UI
 }
+
+initSite();
 
 /** Uses the loaded products data to create a visible product list on the website */
 function addProductsToWebpage() {
     // Check your console to see that the products are stored in the listOfProducts varible.
-    console.log(listOfProducts);
-
-    // Add your code here, remember to brake your code in to smaller function blocks
-    // to reduce complexity and increase readability. Each function should have
-    // an explainetory comment like the one for this function, see row 22.
     
-    // TODO: Remove the console.log and these comments when you've read them.
-}
+    console.log(listOfProducts);
+    /*
+    for(prod in listOfProducts){
+        listOfProducts.filter(function(item, index){
+            console.log(item, 'what')
+            console.log(index, 'topchats')
+            var findFirst = listOfProducts.at(0);
+            console.log(listOfProducts.at(3), 'FOUND IT!!!!!!!!!')
+            let addedPhone = listOfProducts.at(3);
+            localStorage.setItem('added', JSON.stringify(addedPhone));
+            let phones = JSON.parse(localStorage.getItem('added'));
+            console.log('*************')
+            console.log('added: ******', phones.price)
+            console.log(phones.price + phones.price)
+            console.log(findFirst, 'DID WE FIND?')
+            
+            
+            sessionStorage.setItem(index == 0, 'phone');
+            let chat = sessionStorage.getItem('phone', 'here is is')
+            console.log(chat, '******')
+            /*
+            for (var i = 0; i < sessionStorage.length; i++){
+                let h =sessionStorage.getItem(sessionStorage.key(i))
+                console.log(h, 'YAYAY')
+            }*/
+
+        }  
+
+
+/** Adds product to shoppingcart by array-index, to be used with add to cart button */
+function addProdToCartByIndex(saved, i){
+    let addedPhone = listOfProducts.at(i);
+            localStorage.setItem(saved, JSON.stringify(addedPhone));
+            console.log('product added to cart: ', addedPhone)
+            
+    }
+
+
+
+
+
+
+function addProduct(saved, i){
+    newName = {'name' : 'stefan'}
+    localStorage.setItem(saved, JSON.stringify(newName));
+    let tester = localStorage.getItem(saved);
+    console.log(JSON.parse(tester));
+    itemArr = loadProducts()
+    for(let x in itemArr){
+        console.log(x + ': ' + listOfProducts[x] + 'ccccccc')
+    }
+    
+    for(let z = 0; z < listOfProducts ; z++) {
+    let addedPhone = listOfProducts.at(i);
+            localStorage.setItem(saved, JSON.stringify(addedPhone));
+            console.log('added phone', addedPhone)
+            
+    }
+    }    
+
+
